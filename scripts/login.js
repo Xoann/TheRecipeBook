c0f567f5237793f003e602d135e6eda37e547dee
Liam Maguire <liammaguire@outlook.com>
2023-09-29 20:26:56 -0400

    Added signing in

--- scripts/login.js
+++ scripts/login.js
@@ -1,50 +1,49 @@
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// // Initialize the FirebaseUI Widget using Firebase.
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function () {
-      // The widget is rendered.
-      // Hide the loader.
-      // document.getElementById("loader").style.display = "none";
-    },
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: "popup",
-  signInSuccessUrl: "../index.html",
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  tosUrl: "<your-tos-url>",
-  // Privacy policy url.
-  privacyPolicyUrl: "<your-privacy-policy-url>",
-};
+// var uiConfig = {
+//   callbacks: {
+//     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+//       // User successfully signed in.
+//       // Return type determines whether we continue the redirect automatically
+//       // or whether we leave that to developer to handle.
+//       return true;
+//     },
+//     uiShown: function () {
+//       // The widget is rendered.
+//       // Hide the loader.
+//       // document.getElementById("loader").style.display = "none";
+//     },
+//   },
+//   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+//   signInFlow: "popup",
+//   signInSuccessUrl: "../index.html",
+//   signInOptions: [
+//     // Leave the lines as is for the providers you want to offer your users.
+//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//   ],
+//   // Terms of service url.
+//   tosUrl: "<your-tos-url>",
+//   // Privacy policy url.
+//   privacyPolicyUrl: "<your-privacy-policy-url>",
+// };
 
-// The start method will wait until the DOM is loaded.
-ui.start("#firebaseui-auth-container", uiConfig);
+// // The start method will wait until the DOM is loaded.
+// ui.start("#firebaseui-auth-container", uiConfig);
 
-// const signOutButton = document.getElementById("signOut");
+// // const signOutButton = document.getElementById("signOut");
 
-// signOutButton.addEventListener("click", () => {
-//   firebase.auth().signOut();
-//   console.log("signed out");
-// });
+// // signOutButton.addEventListener("click", () => {
+// //   firebase.auth().signOut();
+// //   console.log("signed out");
+// // });
 
 // Sign in/up interactivity
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
-const container = document.getElementById("login-container");
+const container = document.getElementById("container");
 
 signUpButton.addEventListener("click", function () {
-  console.log("h");
   container.classList.add("right-panel-active");
 });
 
@@ -52,4 +51,72 @@ signInButton.addEventListener("click", () =>
   container.classList.remove("right-panel-active")
 );
 
-// BUG nothing happens on button click
+const signUpAccountButton = document.getElementById("signUpAccount");
+const signInAccountButton = document.getElementById("signInAccount");
+
+signUpAccountButton.addEventListener("click", function () {
+  const email = document.getElementById("sign-up-email").value;
+  const passwordOne = document.getElementById("sign-up-password-one").value;
+  const passwordTwo = document.getElementById("sign-up-password-Two").value;
+  const username = document.getElementById("sign-up-username").value;
+  const name = document.getElementById("sign-up-name").value;
+
+  signUpUser(email, password, username, name);
+});
+
+signInAccountButton.addEventListener("click", function () {
+  const email = document.getElementById("sign-in-email").value;
+  // console.log(email);
+  const password = document.getElementById("sign-in-password").value;
+
+  signInUser(email, password);
+});
+
+function signUpUser(email, password, username, name) {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      // Update additional user information (username, name, etc.) in Firestore or Realtime Database
+      // For example, if you are using Firestore:
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          email: email,
+          username: username,
+          name: name,
+        })
+        .then(() => {
+          // User data saved successfully
+          console.log("User signed up successfully!");
+        })
+        .catch((error) => {
+          // Handle errors while saving additional user information
+          console.error("Error saving user data: ", error);
+        });
+    })
+    .catch((error) => {
+      // Handle errors during user registration
+      console.error("Error signing up: ", error);
+    });
+}
+
+// BUG you have to sign in twice
+function signInUser(email, password) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.location.replace("../index.html");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+}

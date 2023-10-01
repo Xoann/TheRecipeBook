d788e9e28fbcda867cb3c1935799d17f9da4a127
Liam Maguire <liammaguire@outlook.com>
2023-10-01 14:59:13 -0400

    Added most sign in/up errors and interactions

--- scripts/login.js
+++ scripts/login.js
@@ -1,43 +1,3 @@
-// // Initialize the FirebaseUI Widget using Firebase.
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-// var uiConfig = {
-//   callbacks: {
-//     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-//       // User successfully signed in.
-//       // Return type determines whether we continue the redirect automatically
-//       // or whether we leave that to developer to handle.
-//       return true;
-//     },
-//     uiShown: function () {
-//       // The widget is rendered.
-//       // Hide the loader.
-//       // document.getElementById("loader").style.display = "none";
-//     },
-//   },
-//   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-//   signInFlow: "popup",
-//   signInSuccessUrl: "../index.html",
-//   signInOptions: [
-//     // Leave the lines as is for the providers you want to offer your users.
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//   ],
-//   // Terms of service url.
-//   tosUrl: "<your-tos-url>",
-//   // Privacy policy url.
-//   privacyPolicyUrl: "<your-privacy-policy-url>",
-// };
-
-// // The start method will wait until the DOM is loaded.
-// ui.start("#firebaseui-auth-container", uiConfig);
-
-// // const signOutButton = document.getElementById("signOut");
-
-// // signOutButton.addEventListener("click", () => {
-// //   firebase.auth().signOut();
-// //   console.log("signed out");
-// // });
-
 // Sign in/up interactivity
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
@@ -80,7 +40,7 @@ signInAccountButton.addEventListener("click", function (event) {
   signInUser(signInEmail, password);
 });
 
-function signUpUser(
+async function signUpUser(
   email,
   passwordOne,
   passwordTwo,
@@ -88,96 +48,210 @@ function signUpUser(
   firstName,
   lastName
 ) {
+  let isError = false;
   // Checking for empty inputs
   if (!email) {
+    isError = true;
+    const otherErrorElement = document.getElementById("errorInvalidEmail");
+    otherErrorElement.classList.add("hide");
     const errorElement = document.getElementById("errorMissingEmail");
     const errorInput = document.getElementById("sign-up-email");
     const errorLabel = document.getElementById("sign-up-email-label");
     handleInputError(errorElement, errorInput, errorLabel);
+  } else if (!isValidEmail(email)) {
+    isError = true;
+    const otherErrorElement = document.getElementById("errorMissingEmail");
+    otherErrorElement.classList.add("hide");
+    const errorElement = document.getElementById("errorInvalidEmail");
+    const errorInput = document.getElementById("sign-up-email");
+    const errorLabel = document.getElementById("sign-up-email-label");
+    handleInputError(errorElement, errorInput, errorLabel);
+  } else {
+    const errorElement = document.getElementById("errorMissingEmail");
+    const errorInput = document.getElementById("sign-up-email");
+    const errorLabel = document.getElementById("sign-up-email-label");
+    removeErrorEffect(errorElement, errorInput, errorLabel);
   }
   if (!firstName) {
+    isError = true;
     const errorElement = document.getElementById("errorMissingFirst");
     const errorInput = document.getElementById("sign-up-first-name");
     const errorLabel = document.getElementById("sign-up-first-name-label");
     handleInputError(errorElement, errorInput, errorLabel);
+  } else {
+    const errorElement = document.getElementById("errorMissingFirst");
+    const errorInput = document.getElementById("sign-up-first-name");
+    const errorLabel = document.getElementById("sign-up-first-name-label");
+    removeErrorEffect(errorElement, errorInput, errorLabel);
   }
   if (!lastName) {
+    isError = true;
     const errorElement = document.getElementById("errorMissingLast");
     const errorInput = document.getElementById("sign-up-last-name");
     const errorLabel = document.getElementById("sign-up-last-name-label");
     handleInputError(errorElement, errorInput, errorLabel);
+  } else {
+    const errorElement = document.getElementById("errorMissingLast");
+    const errorInput = document.getElementById("sign-up-last-name");
+    const errorLabel = document.getElementById("sign-up-last-name-label");
+    removeErrorEffect(errorElement, errorInput, errorLabel);
   }
   if (!username) {
+    isError = true;
+
+    const otherErrorElement = document.getElementById("errorUsernameTaken");
+    otherErrorElement.classList.add("hide");
+
     const errorElement = document.getElementById("errorMissingUser");
     const errorInput = document.getElementById("sign-up-username");
     const errorLabel = document.getElementById("sign-up-username-label");
     handleInputError(errorElement, errorInput, errorLabel);
-  } else if (isUsernameTaken(username)) {
+  } else if (await isUsernameTaken(username)) {
+    isError = true;
+
+    const otherErrorElement = document.getElementById("errorMissingUser");
+    otherErrorElement.classList.add("hide");
+
     const errorElement = document.getElementById("errorUsernameTaken");
     const errorInput = document.getElementById("sign-up-username");
     const errorLabel = document.getElementById("sign-up-username-label");
     handleInputError(errorElement, errorInput, errorLabel);
+  } else {
+    const otherErrorElement = document.getElementById("errorMissingUser");
+    otherErrorElement.classList.add("hide");
+    const errorElement = document.getElementById("errorUsernameTaken");
+    const errorInput = document.getElementById("sign-up-username");
+    const errorLabel = document.getElementById("sign-up-username-label");
+    removeErrorEffect(errorElement, errorInput, errorLabel);
   }
+
+  let isPasswordMissingError = false;
+  let isConfirmMissingError = false;
+
   if (!passwordOne) {
+    isError = true;
+    isPasswordMissingError = true;
+
+    makeSuggestionError();
+
     const errorElement = document.getElementById("errorMissingPassword");
     const errorInput = document.getElementById("sign-up-password-one");
     const errorLabel = document.getElementById("sign-up-password-one-label");
     handleInputError(errorElement, errorInput, errorLabel);
+  } else {
+    const errorElement = document.getElementById("errorMissingPassword");
+    const errorInput = document.getElementById("sign-up-password-one");
+    const errorLabel = document.getElementById("sign-up-password-one-label");
+    removeErrorEffect(errorElement, errorInput, errorLabel);
   }
 
   if (!passwordTwo) {
+    isError = true;
+    isConfirmMissingError = true;
     const errorElement = document.getElementById("errorMissingConfirm");
     const errorInput = document.getElementById("sign-up-password-two");
     const errorLabel = document.getElementById("sign-up-password-two-label");
     handleInputError(errorElement, errorInput, errorLabel);
+  } else {
+    const errorElement = document.getElementById("errorMissingConfirm");
+    const errorInput = document.getElementById("sign-up-password-two");
+    const errorLabel = document.getElementById("sign-up-password-two-label");
+    removeErrorEffect(errorElement, errorInput, errorLabel);
   }
 
-  if (passwordOne !== passwordTwo) {
-  }
+  if (passwordOne !== passwordTwo && !isConfirmMissingError) {
+    isError = true;
+    const errorElement = document.getElementById("errorPasswordDontMatch");
+    errorElement.classList.add("error-message");
 
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, passwordOne)
-    .then((userCredential) => {
-      // Signed up
-      const user = userCredential.user;
-      firebase
-        .firestore()
-        .collection("fastData")
-        .doc("usernames")
-        .get()
-        .then(function (doc) {
-          let currUsernames = doc.data().usernames || [];
-          currUsernames.push(username);
+    const errorInput = document.getElementById("sign-up-password-two");
+    const errorLabel = document.getElementById("sign-up-password-two-label");
 
-          firebase.firestore().collection("fastData").doc("usernames").update({
-            usernames: currUsernames,
-          });
-        });
-
-      firebase
-        .firestore()
-        .collection("users")
-        .doc(user.uid)
-        .set({
-          email: email,
-          username: username,
-          firstName: firstName,
-          lastName: lastName,
-        })
-        .then(() => {
-          // User data saved successfully
-          console.log("User signed up successfully!");
-        })
-        .catch((error) => {
-          // Handle errors while saving additional user information
-          console.error("Error saving user data: ", error);
-        });
-    })
-    .catch((error) => {
-      // Handle errors during user registration
-      console.error("Error signing up: ", error);
+    errorInput.classList.add("error-input");
+    errorLabel.classList.add("error-label");
+    document.getElementById("signIn").addEventListener("click", () => {
+      errorInput.classList.remove("error-input");
+      errorLabel.classList.remove("error-label");
     });
+  } else if (passwordIsNotComplex(passwordOne) && !isPasswordMissingError) {
+    isError = true;
+
+    makeSuggestionError();
+    const errorInput = document.getElementById("sign-up-password-one");
+    const errorLabel = document.getElementById("sign-up-password-one-label");
+
+    errorInput.classList.add("error-input");
+    errorLabel.classList.add("error-label");
+
+    document.getElementById("signIn").addEventListener("click", () => {
+      errorInput.classList.remove("error-input");
+      errorLabel.classList.remove("error-label");
+    });
+  }
+
+  if (!isError) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, passwordOne)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        firebase
+          .firestore()
+          .collection("fastData")
+          .doc("usernames")
+          .get()
+          .then(function (doc) {
+            let currUsernames = doc.data().usernames || [];
+            currUsernames.push(username);
+
+            firebase
+              .firestore()
+              .collection("fastData")
+              .doc("usernames")
+              .update({
+                usernames: currUsernames,
+              });
+          });
+
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(user.uid)
+          .set({
+            email: email,
+            username: username,
+            firstName: firstName,
+            lastName: lastName,
+          })
+          .then(() => {
+            // User data saved successfully
+            console.log("User signed up successfully!");
+          })
+          .catch((error) => {
+            // Handle errors while saving additional user information
+            console.error("Error saving user data: ", error);
+          });
+      })
+      .catch((error) => {
+        // Handle errors during user registration
+        console.error("Error signing up: ", error);
+      });
+  }
+}
+
+function makeSuggestionError() {
+  const suggestions = document.getElementsByClassName("suggestion");
+  for (const suggestionElement of suggestions) {
+    suggestionElement.classList.add("error-message");
+    if (!suggestionElement.classList.contains("hide")) {
+      document
+        .getElementById("signIn")
+        .addEventListener("click", () =>
+          suggestionElement.classList.remove("error-message")
+        );
+    }
+  }
 }
 
 async function signInUser(email, password) {
@@ -189,7 +263,7 @@ async function signInUser(email, password) {
     console.log(user);
     window.location.href = "../index.html";
   } catch (error) {
-    // console.log(error.code);
+    console.log(error.code);
 
     if (error.code === "auth/invalid-email") {
       const errorElement = document.getElementById("errorInvalidEmail");
@@ -200,6 +274,7 @@ async function signInUser(email, password) {
       const errorElement = document.getElementById("errorUserPasswordWrong");
       const errorInput = document.getElementById("sign-in-password");
       const errorLabel = document.getElementById("sign-in-password-label");
+      errorInput.value = "";
       handleInputError(errorElement, errorInput, errorLabel);
     } else if (error.code === "auth/user-not-found") {
       const errorElement = document.getElementById("errorUserNotFound");
@@ -212,20 +287,35 @@ async function signInUser(email, password) {
 }
 
 function handleInputError(errorElement, errorInput, errorLabel) {
-  errorInput.value = "";
+  // errorInput.value = "";
   errorElement.classList.remove("hide");
   errorInput.classList.add("error-input");
   errorLabel.classList.add("error-label");
 
   errorInput.addEventListener("focus", function () {
-    errorElement.classList.add("hide");
-    errorInput.classList.remove("error-input");
-    errorLabel.classList.remove("error-label");
+    // removeErrorEffect(errorElement, errorInput, errorLabel);
   });
+
+  document
+    .getElementById("signIn")
+    .addEventListener("click", () =>
+      removeErrorEffect(errorElement, errorInput, errorLabel)
+    );
+  document
+    .getElementById("signUp")
+    .addEventListener("click", () =>
+      removeErrorEffect(errorElement, errorInput, errorLabel)
+    );
+}
+
+function removeErrorEffect(errorElement, errorInput, errorLabel) {
+  errorElement.classList.add("hide");
+  errorInput.classList.remove("error-input");
+  errorLabel.classList.remove("error-label");
 }
 
 function isUsernameTaken(username) {
-  firebase
+  const taken = firebase
     .firestore()
     .collection("fastData")
     .doc("usernames")
@@ -234,4 +324,148 @@ function isUsernameTaken(username) {
       let currUsernames = doc.data().usernames;
       return currUsernames.includes(username);
     });
+  return taken;
+}
+
+function passwordIsNotComplex(password) {
+  const specialCharacters = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  const numberRegex = /\d/;
+  const upperCaseRegex = /[A-Z]/;
+
+  let isCharPresent = specialCharacters.some(function (char) {
+    return password.includes(char);
+  });
+
+  if (password.length < 8) {
+    return "errorPasswordTooShort";
+  } else if (!upperCaseRegex.test(password)) {
+    return "errorPasswordNoUppercase";
+  } else if (!numberRegex.test(password)) {
+    return "errorNoNumber";
+  } else if (!isCharPresent) {
+    return "errorNoSpecialCharacter";
+  } else {
+    return "";
+  }
+}
+
+let currSuggestion = "";
+
+const signUpPasswordOne = document.getElementById("sign-up-password-one");
+signUpPasswordOne.addEventListener("input", function () {
+  const password = signUpPasswordOne.value;
+  const passwordComplexity = passwordIsNotComplex(password);
+  const errorPasswordTooShortSpan = document.getElementById(
+    "errorPasswordTooShort"
+  );
+  const errorPasswordNoUppercaseSpan = document.getElementById(
+    "errorPasswordNoUppercase"
+  );
+  const errorPasswordNoNumberSpan = document.getElementById("errorNoNumber");
+  const errorNoSpecialCharacterSpan = document.getElementById(
+    "errorNoSpecialCharacter"
+  );
+
+  const otherErrorElement = document.getElementById("errorMissingPassword");
+  otherErrorElement.classList.add("hide");
+
+  if (!password) {
+    errorPasswordTooShortSpan.classList.add("hide");
+    errorPasswordNoUppercaseSpan.classList.add("hide");
+    errorPasswordNoNumberSpan.classList.add("hide");
+    errorNoSpecialCharacterSpan.classList.add("hide");
+  } else if (
+    passwordComplexity === "errorPasswordTooShort" &&
+    currSuggestion !== "errorPasswordTooShort"
+  ) {
+    errorPasswordTooShortSpan.classList.remove("hide");
+    errorPasswordNoUppercaseSpan.classList.add("hide");
+    errorPasswordNoNumberSpan.classList.add("hide");
+    errorNoSpecialCharacterSpan.classList.add("hide");
+  } else if (
+    passwordComplexity === "errorPasswordNoUppercase" &&
+    currSuggestion !== "errorPasswordTooShort"
+  ) {
+    errorPasswordTooShortSpan.classList.add("hide");
+    errorPasswordNoUppercaseSpan.classList.remove("hide");
+    errorPasswordNoNumberSpan.classList.add("hide");
+    errorNoSpecialCharacterSpan.classList.add("hide");
+  } else if (
+    passwordComplexity === "errorNoNumber" &&
+    currSuggestion !== "errorPasswordTooShort"
+  ) {
+    errorPasswordTooShortSpan.classList.add("hide");
+    errorPasswordNoUppercaseSpan.classList.add("hide");
+    errorPasswordNoNumberSpan.classList.remove("hide");
+    errorNoSpecialCharacterSpan.classList.add("hide");
+  } else if (
+    passwordComplexity === "errorNoSpecialCharacter" &&
+    currSuggestion !== "errorPasswordTooShort"
+  ) {
+    errorPasswordTooShortSpan.classList.add("hide");
+    errorPasswordNoUppercaseSpan.classList.add("hide");
+    errorPasswordNoNumberSpan.classList.add("hide");
+    errorNoSpecialCharacterSpan.classList.remove("hide");
+  } else {
+    errorPasswordTooShortSpan.classList.add("hide");
+    errorPasswordNoUppercaseSpan.classList.add("hide");
+    errorPasswordNoNumberSpan.classList.add("hide");
+    errorNoSpecialCharacterSpan.classList.add("hide");
+  }
+});
+
+const signUpPasswordTwo = document.getElementById("sign-up-password-two");
+
+signUpPasswordTwo.addEventListener("input", function () {
+  const password = signUpPasswordOne.value;
+  const confirm = signUpPasswordTwo.value;
+  const errorElement = document.getElementById("errorPasswordDontMatch");
+
+  if (!confirm || passwordsMatch(password, confirm)) {
+    errorElement.classList.add("hide");
+  } else {
+    errorElement.classList.remove("hide");
+  }
+});
+
+function passwordsMatch(password, confirm) {
+  return password === confirm;
+}
+
+function isValidEmail(email) {
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }

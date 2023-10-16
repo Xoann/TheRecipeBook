db7a4a2d57cbbef6889420cf2eecdc134648e305
Liam Maguire <liammaguire@outlook.com>
2023-10-16 18:12:11 -0400

    Fixed an image loading issue when deleting recipes Adds random recipe when you press =

--- scripts/readrecipe.js
+++ scripts/readrecipe.js
@@ -92,6 +92,7 @@ async function getImageURLs(image_names, uid) {
 }
 
 function displayRecipes(recipeNames) {
+  console.log(recipeNames);
   const imageURLs = getImageURLs(recipeNames, currentUid);
 
   for (let i = 0; i < recipeNames.length; i++) {
@@ -562,10 +563,6 @@ function handleDeleteRecipe(recipeName) {
   deleteAcountBtn.addEventListener("click", () => {
     deleteRecipe(recipeName);
     closeModal(modal);
-
-    // delete recipes.recipeName;
-
-    // updateRecipes(narrowSearch(searchQuery));
   });
 }
 
@@ -577,6 +574,8 @@ function deleteRecipe(recipe) {
     .remove()
     .then(function () {
       console.log("Element removed successfully!");
+      delete recipes[recipe];
+      updateRecipes(narrowSearch(searchQuery));
     })
     .catch(function (error) {
       console.error("Error removing element: " + error.message);
@@ -690,3 +689,95 @@ function deleteAcount(uid) {
 
 // BUG Shopping list doesnt reset when you select a recipe after searching it
 // BUG Checkboxes don't data persist when searching recipes
+
+// For testing to auto add new recipes by pressing =
+function writeUserData(
+  recipeName,
+  recipeDesc,
+  cookTimeHrs,
+  cookTimeMins,
+  prepTimeHrs,
+  prepTimeMins,
+  servings,
+  ingredients,
+  steps,
+  image
+) {
+  firebase
+    .database()
+    .ref(`${firebase.auth().currentUser.uid}/recipes/${recipeName}`)
+    .set({
+      recipeDesc: recipeDesc,
+      cookTimeHrs: cookTimeHrs,
+      cookTimeMins: cookTimeMins,
+      prepTimeHrs: prepTimeHrs,
+      prepTimeMins: prepTimeMins,
+      servings: servings,
+      ingredients: ingredients,
+      steps: steps,
+    });
+  const storageRef = firebase.storage().ref();
+  const imageRef = storageRef.child(
+    `${firebase.auth().currentUser.uid}/images/${recipeName}`
+  );
+
+  if (image) {
+    imageRef.put(image).then((snapshot) => {});
+  } else {
+    const imageUrl = "../../img/food-placeholder-1.jpg";
+    fetch(imageUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Create a File object from the Blob
+        const fileObject = new File([blob], "image.png", { type: "image/png" });
+        imageRef.put(fileObject);
+      });
+  }
+  console.log("Uploaded");
+}
+
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "=" || event.code === "Add") {
+    const randomName = generateRandomString(5);
+    const image = "";
+    writeUserData(
+      randomName,
+      "Cheesy garlic chicken bites cooked in one pan with broccoli and spinach in under 15 minutes. This quick tasty dish is a great keto option and can be served with zoodles or pasta!",
+      "1",
+      "15",
+      "0",
+      "5",
+      "4",
+      [
+        { name: "chicken breasts", unit: "lbs", value: "2" },
+        { name: "crushed pepper", unit: "tps.", value: "0.25" },
+        { name: "salt and pepper", unit: "tps.", value: "1" },
+        { name: "garlic", unit: "cloves", value: "3" },
+        { name: "broccoli florets", unit: "cup", value: "2" },
+        { name: "tomatoes", unit: "cup", value: "0.5" },
+        { name: "baby spinach", unit: "cup", value: "2" },
+        { name: "shredded cheese", unit: "cup", value: "0.5" },
+        { name: "cream cheese", unit: "oz", value: "4" },
+      ],
+      [
+        "Heat 2 tablespoons olive oil in a large saucepan over medium-high heat. Add the chopped chicken breasts, season with Italian seasoning, crushed red pepper, and salt & pepper",
+        "Sautee for 4-5 minutes or until chicken is golden and cooked through.",
+        "Add the garlic and saute for another minute or until fragrant. Add the tomato, broccoli, spinach, shredded cheese, and cream cheese.",
+        "Cook for another 3-4 minutes or until the broccoli is cooked through.",
+        "Serve with cooked pasta, rice, zucchini noodles or cauliflower rice.",
+      ],
+      image
+    );
+  }
+});

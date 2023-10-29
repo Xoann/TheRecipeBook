d6bbe685123ee831eba3d1ae91d1a2a1ca9d8d8c
Liam Maguire <liammaguire@outlook.com>
2023-10-28 21:30:26 -0400

    Fixed nav

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -5,23 +5,27 @@ if (darkMode === "enabled") {
   disableDarkMode();
 }
 
-document
-  .querySelector("#toggle-dark-mode")
-  .addEventListener("click", function () {
-    darkMode = localStorage.getItem("darkMode");
-    if (darkMode === "enabled") {
-      disableDarkMode();
-    } else {
-      enableDarkMode();
-    }
-  });
+document.querySelector("#darkmode").addEventListener("click", function () {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode === "enabled") {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+});
 
 function enableDarkMode() {
   localStorage.setItem("darkMode", "enabled");
   document.body.classList.remove("lightMode");
+  document.getElementById("sun").style.display = "block";
+  document.getElementById("moon").style.display = "none";
+  document.getElementById("toggle-dark-mode").textContent = "Light Mode";
 }
 
 function disableDarkMode() {
   localStorage.setItem("darkMode", "disabled");
   document.body.classList.add("lightMode");
+  document.getElementById("toggle-dark-mode").textContent = "Dark Mode";
+  document.getElementById("sun").style.display = "none";
+  document.getElementById("moon").style.display = "block";
 }

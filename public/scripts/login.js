55276af44082dfa3e06a1d5e1d2d0cfbd236ea63
gregoryyoung617 <gregoryyoung617@gmail.com>
2023-11-03 21:40:24 -0400

    made login work with mobile

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -4,6 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
   firebase.auth().signOut();
 });
 
+window.addEventListener("load", function () {
+  document
+    .getElementById("sign-up-view")
+    .classList.remove("mobile-view-btn-active");
+  document
+    .getElementById("sign-in-view")
+    .classList.add("mobile-view-btn-active");
+  document.getElementById("mobile-sign-in").classList.add("mobile-active");
+  document.getElementById("mobile-sign-up").classList.remove("mobile-active");
+  document.getElementById("sign-in-part-doohickey").style.borderRadius =
+    "0px 20px 20px 20px";
+});
+
 // Sign in/up interactivity
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
